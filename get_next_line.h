80f79b651ce061588af00dcfda38af0f17c3fc68
DefectiveRussian <deni.x9@gmail.com>
2024-12-12 13:44:22 +0100

    snap

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,25 @@
 /*   By: dekhamid <dekhamid@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:25:55 by dekhamid          #+#    #+#             */
-/*   Updated: 2024/12/10 11:25:56 by dekhamid         ###   ########.fr       */
+/*   Updated: 2024/12/12 13:43:51 by dekhamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE = 42
+# endif
+
+char	*get_next_line(int fd);
+
+
+#endif
